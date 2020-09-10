e5cee404f90b4add9aca625d13d2ec1e146b41c2
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-09 21:44:32 -0400

    added jQuery and got started with making it work
    Spent some time researching the syntax and setting it up before getting it to work.

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 "use strict";
 
+$(document).ready(execute());
+
+function execute(){
+  $.ajax({
+    url: `http://www.devcodecampmusiclibrary.com/api/music`,
+    dataType: "json",
+    type: "get",
+    success: function (data, textStatus, jqXHR){
+      console.log('Got it.')
+    },
+    error: function (jqXHR, textStatus, errorThrown){
+      // throw("...")
+      console.log(errorThrown);
+    }
+  });
+}
