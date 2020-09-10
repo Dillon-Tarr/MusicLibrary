ed6f173e594aab4a46b6032ceb66208893abf8a0
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-09 22:59:41 -0400

    got the Music Library data to display in an html table using jQuery

--- app.js
+++ app.js
@@ -8,7 +8,18 @@ function execute(){
     dataType: "json",
     type: "get",
     success: function (data, textStatus, jqXHR){
-      console.log('Got it.')
+      for(let i = 0; i < data.length; i++){
+        $("#table-data").append(`
+          <tr>
+            <td>${data[i].id}</td>
+            <td>${data[i].title}</td>
+            <td>${data[i].album}</td>
+            <td>${data[i].artist}</td>
+            <td>${data[i].genre}</td>
+            <td>${data[i].releaseDate}</td>
+          </tr>`
+        );
+      }
     },
     error: function (jqXHR, textStatus, errorThrown){
       // throw("...")
