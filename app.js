32223ed5a53a1d1c952b29354376fd9395c0c39f
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-10 23:26:23 -0400

    user can now filter by a criterion

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 "use strict";
 
-$(document).ready(execute());
+$(document).ready(getAndDisplayFilteredData());
 
-function execute(){
+let filterForm = document.getElementById("filter-form");
+filterForm.onsubmit = function(e){
+  e.preventDefault();
+  var filterBy = filterForm.filterBy.value;
+  var filterText = filterForm.filterText.value;
+  return getAndDisplayFilteredData(filterBy, filterText);
+}
+
+function getAndDisplayFilteredData(filterBy='all-tracks', filterText=''){
   $.ajax({
     url: `http://www.devcodecampmusiclibrary.com/api/music`,
     dataType: "json",
     type: "get",
     success: function (data, textStatus, jqXHR){
-      for(let i = 0; i < data.length; i++){
+      $('#table-data').html('');
+      let filteredData = filterData(data, filterBy, filterText);
+      for(let i = 0; i < filteredData.length; i++){
         if (i % 12 === 0 && i !== 0){
           $("#table-data").append(`
           <tr>
@@ -20,36 +30,36 @@ function execute(){
             <th>Release Date</th>
           </tr>
           <tr class="odd-row">
-            <td>${data[i].id}</td>
-            <td>${data[i].title}</td>
-            <td>${data[i].album}</td>
-            <td>${data[i].artist}</td>
-            <td>${data[i].genre}</td>
-            <td>${data[i].releaseDate}</td>
+            <td>${filteredData[i].id}</td>
+            <td>${filteredData[i].title}</td>
+            <td>${filteredData[i].album}</td>
+            <td>${filteredData[i].artist}</td>
+            <td>${filteredData[i].genre}</td>
+            <td>${filteredData[i].releaseDate}</td>
           </tr>`
         );
         }
         else if (i % 2 === 0){
           $("#table-data").append(`
           <tr class="odd-row">
-            <td>${data[i].id}</td>
-            <td>${data[i].title}</td>
-            <td>${data[i].album}</td>
-            <td>${data[i].artist}</td>
-            <td>${data[i].genre}</td>
-            <td>${data[i].releaseDate}</td>
+            <td>${filteredData[i].id}</td>
+            <td>${filteredData[i].title}</td>
+            <td>${filteredData[i].album}</td>
+            <td>${filteredData[i].artist}</td>
+            <td>${filteredData[i].genre}</td>
+            <td>${filteredData[i].releaseDate}</td>
           </tr>`
         );
         }
         else {
           $("#table-data").append(`
           <tr class="even-row">
-            <td>${data[i].id}</td>
-            <td>${data[i].title}</td>
-            <td>${data[i].album}</td>
-            <td>${data[i].artist}</td>
-            <td>${data[i].genre}</td>
-            <td>${data[i].releaseDate}</td>
+            <td>${filteredData[i].id}</td>
+            <td>${filteredData[i].title}</td>
+            <td>${filteredData[i].album}</td>
+            <td>${filteredData[i].artist}</td>
+            <td>${filteredData[i].genre}</td>
+            <td>${filteredData[i].releaseDate}</td>
           </tr>`
         );
         }
@@ -59,4 +69,25 @@ function execute(){
       console.log(errorThrown);
     }
   });
+}
+
+function filterData(data, filterBy='all-tracks', filterText=''){
+  let dataToFilter = [...data];
+  if (filterText === '' || filterBy === 'all-tracks'){
+    let filteredData = [...dataToFilter];
+    return filteredData;
+  }
+  else if (filterBy === 'id'){
+    let filteredData = []
+    filteredData.push(dataToFilter.find(el => el['id'] === parseInt(filterText)));
+    return filteredData;
+  }
+  else {
+    let filteredData = myFilter(dataToFilter, filterBy, filterText);
+    return filteredData;
+  }
+}
+
+function myFilter(array, criterion, userInput){
+  return array.filter(el => el[criterion].toLowerCase().includes(userInput.toLowerCase()));
 }
